8c419c2c348f8aaa7c4726f2472a060a7a6fd7dd
FrancescaBlidar <francescablidar10@gmail.com>
2020-04-08 14:37:31 +0100

    Added small changes

--- mainSite/urls.py
+++ mainSite/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('/student/', views.student),
+    path('/adjustTicket/', views.adjustTicket),
+    path('/cancelTicket/', views.cancelTicket),
+    path('/bookTicket/', views.bookTicket)
 ]
--- mainSite/views.py
+++ mainSite/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'mainSite/login.html')
+
+def student(request):
+    return render(request, 'mainSite/student.html')
+
+def adjustTicket(request):
+    return render(request, 'mainSite/adjustTicket.html')
+
+def cancelTicket(request):
+    return render(request, 'mainSite/cancelTicket.html')
+
+def bookTicket(request):
+    return render(request, 'mainSite/bookTicket.html')
