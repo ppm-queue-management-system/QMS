c77b79b88b4d4390be7c95a073dbf75f91134b09
Rohit Koanreddi <N0804090>
2020-05-03 20:01:00 +0100

    added links and refactored

--- mainSite/urls.py
+++ mainSite/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 #    path('', views.login),
-    path('', views.SigninView.as_view()),
-    path('administrator/', views.administrator),
-    path('operator/', views.operator),
+    path('', views.SigninView.as_view(), name='login'),
+    path('administrator/', views.administrator, name = 'administrator'),
+    path('operator/', views.operator, name = 'operator'),
     path('student/', views.student),
     path('bookTicket/', views.BookTicketView.as_view()),
     path('adjustTicket/', views.AdjustTicketView.as_view()),
